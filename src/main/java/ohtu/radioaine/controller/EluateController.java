ecae6be5dcd4e0de084c684078e6a4ea07446251
jarno <jarnokor@dhp-2.cs.helsinki.fi>
2012-03-08 10:47:09 +0200

    createEluate function created

--- src/main/java/ohtu/radioaine/controller/EluateController.java
+++ src/main/java/ohtu/radioaine/controller/EluateController.java
@@ -1,12 +1,18 @@
 package ohtu.radioaine.controller;
 
+import java.awt.Event;
+import java.beans.EventHandler;
 import javax.validation.Valid;
+import ohtu.radioaine.domain.Batch;
 import ohtu.radioaine.domain.Eluate;
 import ohtu.radioaine.domain.EluateFormObject;
+import ohtu.radioaine.service.BatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ohtu.radioaine.service.EluateService;
+import ohtu.radioaine.service.EventService;
+import ohtu.radioaine.tools.Time;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,6 +26,10 @@ public class EluateController {
 
     @Autowired
     private EluateService eluateService;
+    @Autowired
+    private BatchService batchService;
+    @Autowired
+    private EventService eventService;
 
     @RequestMapping("createEluate")
     public String createEluate() {
@@ -38,11 +48,34 @@ public class EluateController {
             Event event = EventHandler.newEluateEvent(Eluate);
             eventService.createOrUpdate(event);
         } else {
-            Eluate = updateEluateSaato(temp.getId(), bfm);
+            Eluate = updateEluateSaato(temp.getId(), efo);
         }
         return "redirect:/Eluate/" + Eluate.getId();
-        return "createEluate";
     }
     
+    /**
+     *
+     * @param efo
+     * @return
+     */
+    private Eluate createEluate(EluateFormObject efo) {
+        Eluate eluate = new Eluate();
+        
+        eluate.setStrength(efo.getStrength());
+        eluate.setVolume(efo.getVolume());
+        eluate.setTimestamp(Time.parseDate(efo.getTimestamp()));
+        eluate.setSignature(efo.getSignature());
+        eluate.setNote(efo.getNote());
+        eluate.setStorageLocation(efo.getStorageLocation());
+        
+        Batch generator = (Batch) batchService.read(efo.getGenerator().getId());
+        Batch solvent = (Batch) batchService.read(efo.getSolvent().getId());
+        eluate.setGenerator(generator);
+        eluate.setSolvent(solvent);
+        
+        eluateService.createOrUpdate(eluate);
+
+        return eluate;
+    }
 
 }

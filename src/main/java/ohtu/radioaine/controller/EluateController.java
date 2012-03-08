88612ad52be576c0ac6d2a461d2c9ef731a926ed
Riku Heinonen <riku.heinonen@helsinki.fi>
2012-03-08 10:21:18 +0200

    eluaattikontrollia säädetty

--- src/main/java/ohtu/radioaine/controller/EluateController.java
+++ src/main/java/ohtu/radioaine/controller/EluateController.java
@@ -1,9 +1,15 @@
 package ohtu.radioaine.controller;
 
+import javax.validation.Valid;
+import ohtu.radioaine.domain.Eluate;
+import ohtu.radioaine.domain.EluateFormObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ohtu.radioaine.service.BatchService;
+import ohtu.radioaine.service.EluateService;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Controllers for eluate creation ja viewing
@@ -13,11 +19,30 @@ import ohtu.radioaine.service.BatchService;
 public class EluateController {
 
     @Autowired
-    private BatchService esinePalvelu;
+    private EluateService eluateService;
 
     @RequestMapping("createEluate")
     public String createEluate() {
         return "createEluate";
     }
+    
+    @RequestMapping(value = "createEluate", method = RequestMethod.POST)
+    public String newEluate(@Valid @ModelAttribute("eluate") EluateFormObject efo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "createEluate";
+        }
+        Eluate eluate = createEluate(efo);
+        Eluate temp = EluateService.read(Eluate.getEluateNumber(), bfm.getSubstance());
+        if (temp == null) {
+            Eluate = EluateService.createOrUpdate(Eluate);
+            Event event = EventHandler.newEluateEvent(Eluate);
+            eventService.createOrUpdate(event);
+        } else {
+            Eluate = updateEluateSaato(temp.getId(), bfm);
+        }
+        return "redirect:/Eluate/" + Eluate.getId();
+        return "createEluate";
+    }
+    
 
 }

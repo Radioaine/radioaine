b6d5e0b1d934112822d0ee7f18eb5044853ce076
Riku Heinonen <Richard Moneybags@RichardMoneybag.(none)>
2012-03-26 21:22:50 +0300

    changed some scripts to work with Interner Exploder, for science

--- src/main/webapp/WEB-INF/script/script.js
+++ src/main/webapp/WEB-INF/script/script.js
@@ -5,14 +5,14 @@ var selectionCounter = 0;
 function addStorage(usedStorageLocationsCount, storageLocationsCount){
     if(usedStorageLocationsCount > 0 && usedStorageLocationsCount >= storageCounter)
         storageCounter = usedStorageLocationsCount + 1;
-    var storage = document.querySelector("#varastot");
+    var storage = document.getElementById("varastot");
     var newdiv = document.createElement("div");
     newdiv.id = "varasto"+storageCounter;
     var newHTML = "<select name=\"storageLocations["+(storageCounter-1)+"][0]\">";
     for(var i = 0; i < storageLocationsCount; i++)  {
         newHTML += "<option value=\""+(i+1)+"\">Jääkaappi "+(i+1)+"</option>";
     }
-    newHTML += "</select> <input onchange=\"countAmount()\" value=\"0\" id=\"storageAmount\" type=\"number\" size=\"3\" name=\"storageLocations["+(storageCounter-1)+"][1] \"> kpl &nbsp; <button type=\"button\" onClick=\"removeStorage("+storageCounter+")\">Poista</button>";
+    newHTML += "</select> <input class=\"temp\" onchange=\"countAmount()\" value=\"0\" id=\"storageAmount\" type=\"number\" size=\"3\" name=\"storageLocations["+(storageCounter-1)+"][1] \"> kpl &nbsp; <button type=\"button\" onClick=\"removeStorage("+storageCounter+")\">Poista</button>";
     newdiv.innerHTML = newHTML;
     storageCounter++;
     storage.appendChild(newdiv);
@@ -20,20 +20,19 @@ function addStorage(usedStorageLocationsCount, storageLocationsCount){
 
 //Ongelma jossa jos lisää vaikka kome ja poistaa keskimmäisen niin numerointi menee sekaisin;
 function removeStorage(number){
-    var storage = document.querySelector("#varastot");
-    var remove = document.querySelector("#varasto"+number);
+    var storage = document.getElementById("varastot");
+    var remove = document.getElementById("varasto"+number);
     storageCounter--;
     storage.removeChild(remove);
     countAmount();
 }
 
 function qualityResults(){
-    var holder = document.querySelector("#qualityCheck");
+    var holder = document.getElementById("qualityCheck");
     holder.innerHTML = "<form action=\"doCheck\"><select name=\"qualityCheck\"><option value=\"1\">Hyväksytty</option><option value=\"2\">Hylätty</option></select>"
 }
 
 function eluateAmounts(e){
-    console.log(e.target.id);
     var newDiv = document.createElement("div");
     newDiv.id = "selection"+selectionCounter;
     var newHTML = "";
@@ -51,28 +50,27 @@ function eluateAmounts(e){
     }
     newDiv.innerHTML = newHTML;
     selectionCounter++;
-    document.querySelector("#selected").appendChild(newDiv);
-    console.log(newDiv);
+    document.getElementById("selected").appendChild(newDiv);
 }
 
 function revealForm(){
-    document.querySelector("#butski").parentNode.removeChild(document.querySelector("#butski"));
-    document.querySelector("#removeForm").style.cssText = "display: inline;";
+    document.getElementById("butski").parentNode.removeChild(document.getElementById("butski"));
+    document.getElementById("removeForm").style.cssText = "display: inline;";
 }
 
 function removeSelection(elem){
     console.log(elem);
-    document.querySelector("#"+elem.id).parentNode.removeChild(elem);
+    document.getElementById(""+elem.id).parentNode.removeChild(elem);
     selectionCounter--;
 }
 
 function countAmount(){
     var total=0;
-    var amounts = document.querySelectorAll("#storageAmount");
+    var amounts = document.getElementsByClassName("temp");
     for(var i=0; i<amounts.length; i++){
         total += parseInt(amounts[i].value);
     }
-    document.querySelector("#t").innerText = total+" kpl";
+    document.getElementById("t").innerText = total+" kpl";
     
 }
 

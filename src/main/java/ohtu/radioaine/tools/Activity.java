84f289631ea093313ce59052048474fa67591068
Suvi Jentze-Korpi <suvi.jentze@helsinki.fi>
2012-04-15 16:16:46 +0300

    modified Activity.java

--- src/main/java/ohtu/radioaine/tools/Activity.java
+++ src/main/java/ohtu/radioaine/tools/Activity.java
@@ -3,33 +3,29 @@ package ohtu.radioaine.tools;
 import java.sql.Timestamp;
 
 /**
- *
+ *  Class for calculating level of eluate radioactivity at given moment
+ *  Used timeunit is seconds
+ * 
  * @author Suvi Jentze-Korpi
  */
 public class Activity {
- 
-    /*Calculate current activity*/     
-    
+
     public double CurrentActivity(Timestamp t0, Timestamp t, int halflife, int strength){
-        int A0 = strength;  //eluate activity at time t0
-        long dT = ElapsedTime(t0,t);    //elapsed time
+        int A0 = strength;              //eluate activity at time t0
+        long dT = ElapsedTime(t0,t);    //elapsed time since creation of eluate
         
         /*Calculate value of lambda*/            
-        double ln2 = Math.log(2);   //natural logarithm of 2
+        double ln2 = Math.log(2);       //natural logarithm of 2
         double lambda = ln2 / halflife; // NB. halflife = ln2/lambda
         
+        /*Calculate current activity*/
         double A = 0;           //calculated activity
         A = CalculateActivity(A, A0,lambda,dT);
         
         return A;   //return current activity
     }
     
-    
-    
     private double CalculateActivity(double A, int A0,double lambda, long dT){
-        
-        
-        
         A = A0*Math.exp((-lambda*dT));
         return A;
     }
@@ -44,6 +40,5 @@ public class Activity {
         long differenceInSeconds = ((difference*10)*10)*10;  //unit is seconds
         return differenceInSeconds;
     }
-    
-    
+  
 }

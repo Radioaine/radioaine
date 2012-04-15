6cb273d92951e3a4350283c2080be8d145fdad66
Suvi Jentze-Korpi <suvi.jentze@helsinki.fi>
2012-04-15 14:54:17 +0300

    added Activity.java for calculating current activity

---
+++ src/main/java/ohtu/radioaine/tools/Activity.java
@@ -0,0 +1,16 @@
+package ohtu.radioaine.tools;
+
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author Suvi Jentze-Korpi
+ */
+public class Activity {
+    double A0;      //eluate activity at time t0
+    double A;       //calculated activity
+    Timestamp t0;   //time of creation of eluate
+    Timestamp t;    //current time
+    
+    
+}

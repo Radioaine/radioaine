f7adf142656f4083783de9f5e7a38f260b6ed919
Suvi Jentze-Korpi <suvi.jentze@helsinki.fi>
2012-04-15 16:11:00 +0300

    modified Activity.java

--- src/main/java/ohtu/radioaine/tools/Activity.java
+++ src/main/java/ohtu/radioaine/tools/Activity.java
@@ -7,10 +7,43 @@ import java.sql.Timestamp;
  * @author Suvi Jentze-Korpi
  */
 public class Activity {
-    double A0;      //eluate activity at time t0
-    double A;       //calculated activity
-    Timestamp t0;   //time of creation of eluate
-    Timestamp t;    //current time
+ 
+    /*Calculate current activity*/     
+    
+    public double CurrentActivity(Timestamp t0, Timestamp t, int halflife, int strength){
+        int A0 = strength;  //eluate activity at time t0
+        long dT = ElapsedTime(t0,t);    //elapsed time
+        
+        /*Calculate value of lambda*/            
+        double ln2 = Math.log(2);   //natural logarithm of 2
+        double lambda = ln2 / halflife; // NB. halflife = ln2/lambda
+        
+        double A = 0;           //calculated activity
+        A = CalculateActivity(A, A0,lambda,dT);
+        
+        return A;   //return current activity
+    }
+    
+    
+    
+    private double CalculateActivity(double A, int A0,double lambda, long dT){
+        
+        
+        
+        A = A0*Math.exp((-lambda*dT));
+        return A;
+    }
+    
+    private long ElapsedTime(Timestamp t0, Timestamp t){
+        // t0 is time of creation of eluate
+        // t is current time
+        
+        long time_t0 = t0.getTime();
+        long time_t = t.getTime();
+        long difference = time_t - time_t0; //unit is milliseconds
+        long differenceInSeconds = ((difference*10)*10)*10;  //unit is seconds
+        return differenceInSeconds;
+    }
     
     
 }
